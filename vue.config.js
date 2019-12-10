8d7817bb7ae1d06e2e7ec5f0fce60d45b5f32bcd
22969541+Aurelius333@users.noreply.github.com <22969541+Aurelius333@users.noreply.github.com>
2019-12-10 18:50:01 -0500

    Add Vue CLI PWA plugin

--- vue.config.js
+++ vue.config.js
@@ -2,4 +2,14 @@ module.exports = {
   configureWebpack: {
     devtool: "source-map",
   },
+  pwa: {
+    iconPaths: {
+      favicon32: "img/icons/favicon-32x32.png",
+      favicon16: "img/icons/favicon-16x16.png",
+      appleTouchIcon: "img/icons/apple-touch-icon.png",
+      maskIcon: "img/icons/safari-pinned-tab.svg",
+      msTileImage: "img/icons/mstile-150x150.png",
+    },
+    themeColor: "#03a1fc",
+  },
 };
