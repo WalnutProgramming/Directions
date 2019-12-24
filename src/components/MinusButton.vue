401cc8f9483277d5676210a751e1ccd98181d997
presssssure <33643911+PressurizedSphere@users.noreply.github.com>
2019-12-24 01:48:05 -0500

    create minus button
    will replace delete button soon

---
+++ src/components/MinusButton.vue
@@ -0,0 +1,46 @@
+<template>
+  <button :type="type" class="minus-button" @click.stop.prevent="$emit('customclick')">
+      <slot></slot>
+  </button>
+</template>
+
+<script lang="ts">
+import Vue from "vue";
+
+export default Vue.extend({
+  props: {
+    type: { type: String, default: "submit" },
+  },
+});
+</script>
+
+<style scoped>
+
+.minus-button {
+  color: red;
+  border: none;
+  height: 6vh;
+  width: 6vh;
+  font-size: 1em;
+
+  background: rgba(255, 120, 120, 0);
+  display: inline-block;
+  border-radius: 50px;
+
+  -webkit-user-select: none; /* Safari 3.1+ */
+  -moz-user-select: none; /* Firefox 2+ */
+  -ms-user-select: none; /* IE 10+ */
+  user-select: none; /* Standard syntax */
+
+  transition: var(--long-decay-ease);
+}
+
+.minus-button:hover {
+  background: rgb(255, 120, 120, 1);
+  color: white;
+  transition: var(--long-decay-ease);
+  cursor: pointer;
+  border-radius: 7.5px;
+}
+
+</style>
