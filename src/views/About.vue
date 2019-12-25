27059e1f35027dc839d5eba43261d99e00718e8b
presssssure <33643911+PressurizedSphere@users.noreply.github.com>
2019-12-24 20:25:46 -0500

    Update About.vue

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,6 +1,73 @@
 <template>
   <div class="about" style="text-align:center">
-    <h1> ⚞Walnut Hills Programming Club⚟ </h1>
-    Walnut Hills is a big and confusing school to navigate.
+    <h1 class="title">A Walnut Hills Programming Club Project</h1>
+    <p class="squished-p">
+      Walnut Hills is a big and confusing school to navigate, especially for
+      incoming students.
+    </p>
+    <h1 class="subtitle">The Site:</h1>
+    <p class="squished-p">
+      Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+      tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
+      veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
+      commodo consequat. Duis aute irure dolor in reprehenderit in voluptate
+      velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat
+      cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id
+      est laborum.
+    </p>
+    <h1 class="subtitle">Our School:</h1>
+    <p class="squished-p">
+      Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+      tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
+      veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
+      commodo consequat. Duis aute irure dolor in reprehenderit in voluptate
+      velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat
+      cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id
+      est laborum.
+    </p>
+    <h1 class="subtitle">Our Club:</h1>
+    <p class="squished-p">
+      Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+      tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
+      veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
+      commodo consequat. Duis aute irure dolor in reprehenderit in voluptate
+      velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat
+      cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id
+      est laborum.
+    </p>
+    <h1 class="subtitle">Our People:</h1>
+    <p class="squished-p">
+      Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+      tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
+      veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
+      commodo consequat. Duis aute irure dolor in reprehenderit in voluptate
+      velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat
+      cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id
+      est laborum.
+    </p>
+    <h1 class="subtitle">Special Thanks to</h1>
+    <p class="squished-p">
+      <a href="https://vuejs.org/">Vue.js</a>, the javascript framework this
+      site was made with <br />
+      <a href="https://overpassfont.org/">Overpass</a>, the open source font
+      used here
+    </p>
   </div>
 </template>
+
+<style scoped>
+.title {
+  font-size: 50px;
+  color: rgb(0, 39, 116);
+}
+
+.subtitle {
+  font-size: 40px;
+  color: rgb(32, 75, 160);
+}
+
+.squished-p {
+  margin-left: 10vw;
+  margin-right: 10vw;
+}
+</style>
