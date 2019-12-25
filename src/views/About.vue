695dda5f2a73ef7014b14387b26030d2e82f13d5
presssssure <33643911+PressurizedSphere@users.noreply.github.com>
2019-12-24 20:46:03 -0500

    Update About.vue

--- src/views/About.vue
+++ src/views/About.vue
@@ -5,7 +5,7 @@
       Walnut Hills is a big and confusing school to navigate, especially for
       incoming students.
     </p>
-    <h1 class="subtitle">The Site:</h1>
+    <h1 class="subtitle">The Project:</h1>
     <p class="squished-p">
       Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
       tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
@@ -35,20 +35,12 @@
       cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id
       est laborum.
     </p>
-    <h1 class="subtitle">Our People:</h1>
-    <p class="squished-p">
-      Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
-      tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
-      veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
-      commodo consequat. Duis aute irure dolor in reprehenderit in voluptate
-      velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat
-      cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id
-      est laborum.
-    </p>
     <h1 class="subtitle">Special Thanks to</h1>
     <p class="squished-p">
-      <a href="https://vuejs.org/">Vue.js</a>, the javascript framework this
+      <a href="https://vuejs.org/">Vue.js</a>, the Javascript framework this
       site was made with <br />
+      <a href="https://www.typescriptlang.org/">TypeScript</a>, an open source
+      programming language this project is written in<br />
       <a href="https://overpassfont.org/">Overpass</a>, the open source font
       used here
     </p>
