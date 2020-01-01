2394021b6787fc5a09fe1752dab4b31db5eb77d4
22969541+Aurelius333@users.noreply.github.com <22969541+Aurelius333@users.noreply.github.com>
2020-01-01 16:48:54 -0500

    Check for new version of site every 45 minutes

--- src/registerServiceWorker.ts
+++ src/registerServiceWorker.ts
@@ -7,8 +7,13 @@ if (process.env.NODE_ENV === "production") {
     ready() {
       console.log("App is being served from cache by a service worker.");
     },
-    registered() {
+    registered(registration: ServiceWorkerRegistration) {
       console.log("Service worker has been registered.");
+      // Check for new version of service worker every 45 minutes
+      setInterval(() => {
+        console.log("Checking for updates");
+        registration.update();
+      }, 1000 * 60 * 45);
     },
     cached() {
       console.log("Content has been cached for offline use.");
