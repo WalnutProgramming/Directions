f6670d699bda0ac9198240f5a70111168fce2943
22969541+Aurelius333@users.noreply.github.com <22969541+Aurelius333@users.noreply.github.com>
2020-01-21 09:52:12 -0500

    Use objectContaining in walnut validity test

--- tests/unit/walnut.spec.ts
+++ tests/unit/walnut.spec.ts
@@ -1,5 +1,9 @@
 import walnut from "@/walnut";
 
 test("walnut is a valid Building", () => {
-  expect(walnut.validity.valid).toBe(true);
+  expect(walnut.validity).toEqual(
+    expect.objectContaining({
+      valid: true,
+    })
+  );
 });
