e7a9dae7fe32dfd94492645ceb704c5681e51276
22969541+Aurelius333@users.noreply.github.com <22969541+Aurelius333@users.noreply.github.com>
2020-02-09 14:29:27 -0500

    Use CSS data properties for dark theme
    instead of dynamically changing styles

--- src/components/TheMoonButton.vue
+++ src/components/TheMoonButton.vue
@@ -9,96 +9,8 @@
 <script lang="ts">
 import Vue from "vue";
 
-const lightTheme = {
-  "--background-color": "#ffffff",
-  "--alt-background-color": "rgb(221, 237, 241)",
-  "--slick-background-color": "#f5f8fa",
-
-  "--button-color": "#03a1fc",
-  "--button-shade-color": "#007dc4",
-  "--button-text-color": "#ffffff",
-  "--button-hold-color": "rgb(209, 226, 228)",
-
-  "--minus-button-color": "rgba(255, 120, 120, 1)",
-  "--minus-button-color-hover": "#ff0000",
-
-  "--plus-button-color": "rgb(120, 200, 120)",
-  "--plus-button-hover-color": "rgb(93, 158, 93)",
-
-  "--edit-button-color": "#ebac00",
-  "--edit-button-shade-color": "#cf8f05",
-  "--edit-button-hold-color": "rgb(228, 231, 206)",
-
-  "--input-text-color": "#000000",
-  "--input-placeholder-text-color": "#a2bace",
-  "--input-selection-color": "#c4e0f0",
-  "--input-selection-text-color": "#000000",
-  "--input-background-color": "#f5f8fa",
-
-  "--heading-text-color": "#220d8a",
-  "--subheading-text-color": "#204ba0",
-  "--par-text-color": "#000000",
-  "--less-important-text-color": "#6f6f6f",
-
-  "--header-bar-text-color": "#0b004b",
-  "--header-bar-selected-text-color": "#2355b8",
-  "--header-bar-hover-text-color": "#03a1fc",
-  "--header-bar-shadow-color": "#03a1fc",
-
-  "--link-color": "#0000ee",
-  "--visited-link-color": "#5a1a8b",
-
-  "--moon-filter": "hue-rotate(0deg) contrast(110%)",
-};
-
-const darkTheme = {
-  "--background-color": "#15202b",
-  "--alt-background-color": "#192734",
-  "--slick-background-color": "#253341",
-
-  "--button-color": "#f0d019",
-  "--button-shade-color": "#c2980f",
-  "--button-text-color": "#2f004e",
-  "--button-hold-color": "#1c2e40",
-
-  "--minus-button-color": "rgba(255, 120, 120, 1)",
-  "--minus-button-color-hover": "#ff0000",
-
-  "--plus-button-color": "rgb(120, 200, 120)",
-  "--plus-button-hover-color": "rgb(93, 158, 93)",
-
-  "--edit-button-color": "#ebac00",
-  "--edit-button-shade-color": "#cf8f05",
-  "--edit-button-hold-color": "#1c2e40",
-
-  "--input-text-color": "#ffffff",
-  "--input-placeholder-text-color": "#f7efa8",
-  "--input-selection-color": "#f7efa8",
-  "--input-selection-text-color": "#2f004e",
-  "--input-background-color": "#253341",
-
-  "--heading-text-color": "#ccc9a9",
-  "--subheading-text-color": "#fffcec",
-  "--par-text-color": "#ffffff",
-  "--less-important-text-color": "#6f6f6f",
-
-  "--header-bar-text-color": "#5e560e",
-  "--header-bar-selected-text-color": "#968d3b",
-  "--header-bar-hover-text-color": "#f0d019",
-  "--header-bar-shadow-color": "#f0ac19",
-
-  "--link-color": "#0087e0",
-  "--visited-link-color": "#9a41ee",
-
-  "--moon-filter": "hue-rotate(180deg) contrast(90%)",
-};
-
-const root = document.documentElement;
-
-function setTheme(theme: Object) {
-  Object.entries(theme).forEach(([key, val]) => {
-    root.style.setProperty(key, val);
-  });
+function setTheme(dark: boolean) {
+  document.documentElement.dataset.theme = dark ? "dark" : "light";
 }
 
 const IS_DARK_THEME_LOCALSTORAGE = "isDarkTheme";
@@ -122,7 +34,7 @@ export default Vue.extend({
       immediate: true,
       handler(newVal, oldVal) {
         if (newVal !== oldVal) {
-          setTheme(newVal ? darkTheme : lightTheme);
+          setTheme(newVal);
         }
         localStorage.setItem(
           IS_DARK_THEME_LOCALSTORAGE,
