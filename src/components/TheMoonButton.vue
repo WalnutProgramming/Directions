5b211d9233d519022bba15324c93a766c6b74637
22969541+Aurelius333@users.noreply.github.com <22969541+Aurelius333@users.noreply.github.com>
2020-02-09 14:47:13 -0500

    Disable transitions while loading page

--- src/components/TheMoonButton.vue
+++ src/components/TheMoonButton.vue
@@ -15,32 +15,28 @@ function setTheme(dark: boolean) {
 
 const IS_DARK_THEME_LOCALSTORAGE = "isDarkTheme";
 
+const stored = localStorage.getItem(IS_DARK_THEME_LOCALSTORAGE);
+const initialDarkTheme: boolean =
+  stored != null
+    ? // If we have the user's preference stored, use that.
+      JSON.parse(stored)
+    : // If the user is using dark mode on their device, use that.
+      window.matchMedia &&
+      window.matchMedia("(prefers-color-scheme: dark)").matches;
+setTheme(initialDarkTheme);
+
 export default Vue.extend({
   data() {
-    // If we have the user's preference stored, use that.
-    let isDarkTheme: boolean | null = JSON.parse(
-      localStorage.getItem(IS_DARK_THEME_LOCALSTORAGE) ?? "null"
-    );
-    if (isDarkTheme == null) {
-      // If the user is using dark mode on their device, use that.
-      isDarkTheme =
-        window.matchMedia &&
-        window.matchMedia("(prefers-color-scheme: dark)").matches;
-    }
-    return { isDarkTheme };
+    return { isDarkTheme: initialDarkTheme };
   },
   watch: {
-    isDarkTheme: {
-      immediate: true,
-      handler(newVal, oldVal) {
-        if (newVal !== oldVal) {
-          setTheme(newVal);
-        }
-        localStorage.setItem(
-          IS_DARK_THEME_LOCALSTORAGE,
-          JSON.stringify(newVal)
-        );
-      },
+    isDarkTheme(isDarkTheme: boolean) {
+      setTheme(isDarkTheme);
+      localStorage.setItem(
+        IS_DARK_THEME_LOCALSTORAGE,
+        JSON.stringify(isDarkTheme)
+      );
+      document.querySelector("body")?.classList.add("enable-transition");
     },
   },
   methods: {
