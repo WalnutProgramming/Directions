d31ac589e239299f25518bbd13cfb71b4835791b
22969541+Aurelius333@users.noreply.github.com <22969541+Aurelius333@users.noreply.github.com>
2020-02-17 10:48:07 -0500

    Prerender / and /about

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,5 @@
 <template>
-  <div>
+  <div id="app" data-view>
     <header>
       <nav>
         <ul>
--- vue.config.js
+++ vue.config.js
@@ -1,7 +1,32 @@
+/* eslint-disable no-param-reassign */
+const path = require("path");
+const PrerenderSpaPlugin = require("prerender-spa-plugin");
+
+const productionPlugins = [
+  new PrerenderSpaPlugin({
+    staticDir: path.join(__dirname, "dist"),
+    routes: ["/", "/about"],
+    postProcess(renderedRoute) {
+      renderedRoute.html = renderedRoute.html
+        .replace(/<script (.*?)>/g, "<script $1 defer>")
+        .replace('id="app"', 'id="app" data-server-rendered="true"');
+
+      return renderedRoute;
+    },
+    renderer: new PrerenderSpaPlugin.PuppeteerRenderer({
+      // We need to inject a value so we're able to
+      // detect if the page is currently pre-rendered.
+      inject: {},
+      // Our view component is rendered after the API
+      // request has fetched all the necessary data,
+      // so we create a snapshot of the page after the
+      // `data-view` attribute exists in the DOM.
+      renderAfterElementExists: "[data-view]",
+    }),
+  }),
+];
+
 module.exports = {
-  configureWebpack: {
-    devtool: "source-map",
-  },
   pwa: {
     iconPaths: {
       favicon32: "img/icons/favicon-32x32.png",
@@ -23,5 +48,11 @@ module.exports = {
     manifestOptions: {
       short_name: "Walnut.Direct",
     },
+  },
+  configureWebpack: config => {
+    config.devtool = "source-map";
+    if (process.env.NODE_ENV === "production") {
+      config.plugins.push(...productionPlugins);
+    }
   },
 };
