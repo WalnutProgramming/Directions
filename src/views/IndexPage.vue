228515f2bb503a832b3aba686e42a7da883fcbc1
annie <68390211+wowannie@users.noreply.github.com>
2021-03-19 19:05:00 -0400

    Update IndexPage.vue
    Sorry if I somehow did that incorrectly!!

--- src/views/IndexPage.vue
+++ src/views/IndexPage.vue
@@ -5,6 +5,10 @@
 
       <main>
         <StatusUpdates />
+        
+        <p> Click 
+	<a href=https://file.groupme.com/v1/46049414/files/a3e56f6c-45ce-4e70-adbc-865be77b3f4e>here</a>
+    for a link to the floor map! <p>
 
         <h1 class="where-question">Where do you need to go?</h1>
         <form id="roomForm">
