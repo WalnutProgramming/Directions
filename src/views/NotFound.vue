cf0edec989f10b65e78845762671391c3c147605
louiske65 <38638922+louiske65@users.noreply.github.com>
2021-04-08 16:58:52 -0400

    Add an image and center text

--- src/views/NotFound.vue
+++ src/views/NotFound.vue
@@ -1,5 +1,8 @@
 <template>
-  <p>404 Page Not Found</p>
+  <div id="main">
+    <p>404 Page Not Found</p>
+    <img src="../assets/WalnutImage.jpg" />
+  </div>
 </template>
 
 <script lang="ts">
@@ -9,3 +12,24 @@ export default Vue.extend({
   metaInfo: { title: "NotFound" },
 });
 </script>
+
+<style scoped>
+#main {
+  width: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+
+img {
+  height: 50%;
+  display: block;
+  margin-left: auto;
+  margin-right: auto;
+  width: 50%;
+}
+
+p {
+  text-align: center;
+}
+</style>
