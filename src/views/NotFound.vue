210f8ca9b64a413137ca2ffc3cf76c4df4567dd5
louiske65 <38638922+louiske65@users.noreply.github.com>
2021-04-08 16:28:32 -0400

    Test 404 Page
    Created test page to see if the correct page displays on 404 error

---
+++ src/views/NotFound.vue
@@ -0,0 +1,11 @@
+<template>
+  <p>404 Page Not Found</p>
+</template>
+
+<script lang="ts">
+import Vue from "vue";
+
+export default Vue.extend({
+  metaInfo: { title: "NotFound" },
+});
+</script>
