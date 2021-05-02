d94e13dfebed6281473cc1e24ddd5ce28d1211ce
louiske65 <38638922+louiske65@users.noreply.github.com>
2021-05-01 21:19:22 -0400

    Update NotFound.vue

--- src/views/NotFound.vue
+++ src/views/NotFound.vue
@@ -2,15 +2,19 @@
   <div id="main">
     <p>404 Page Not Found</p>
     <img src="../assets/WalnutImage.jpg" />
-    <button @click="$router.push('/')">Go back to the home page</button>
+    <CustomButton id="button" @click="$router.push('/')"
+      >Go back to the home page</CustomButton
+    >
   </div>
 </template>
 
 <script lang="ts">
 import Vue from "vue";
+import CustomButton from "@/components/buttons/CustomButton.vue";
 
 export default Vue.extend({
   metaInfo: { title: "Not Found" },
+  components: { CustomButton },
 });
 </script>
 
@@ -29,11 +33,9 @@ img {
   height: 50%;
 }
 
-button {
-  background-color: var(--button-color);
-  color: var(--button-text-color);
-  margin-top: 1em;
-  font-size: 0.75em;
+#button {
+  margin-top: 0.75em;
+  font-size: 0.5em;
 }
 
 p {
