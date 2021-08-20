282a2376aa172e61c3a0bd247274255471476193
22969541+Aurelius333@users.noreply.github.com <22969541+Aurelius333@users.noreply.github.com>
2021-08-19 23:20:09 -0400

    Make Jest work

--- jest.config.js
+++ jest.config.js
@@ -1,4 +1,6 @@
 module.exports = {
   // preset: "@vue/cli-plugin-unit-jest/presets/typescript-and-babel",
   preset: "vite-jest",
+  testMatch: ["**/tests/unit/**/*.spec.[jt]s?(x)", "**/__tests__/*.[jt]s?(x)"],
+  testEnvironment: "jsdom",
 };
